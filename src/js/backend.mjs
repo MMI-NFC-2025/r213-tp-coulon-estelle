86ce2e13879b9cf348ee698475bbda866ba64c38
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2026-02-27 09:34:18 +0100

    stylisation des choses ajoutées

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -17,3 +17,32 @@ export async function getOffres() {
 export async function getImageUrl(record, recordImage) {
     return db.files.getURL(record, recordImage);
 }
+
+export async function addOffre(house) {
+    try {
+        await db.collection('maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
+
+export async function filterByPrix(minPrix, maxPrix) {
+    try {
+        let data = await db.collection('maison').getFullList({
+            filter: `prix >= ${minPrix} && prix <= ${maxPrix}`,
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant par prix', error);
+        return [];
+    }
+}
